5bf949e432a6e0a4fa7ff78cbaca1f99b3c5ad40
Barton Schaefer <shoredata@gmail.com>
2018-07-05 12:40:55 -0700

    Cars and cats.

---
+++ cars_and_cats/app.js
@@ -0,0 +1,118 @@
+// get the http module:copy
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+
+    if(request.url === '/') { // request.method == 'GET'
+        fs.readFile('./views/cars.html', 'utf8', function (errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/cars') {
+        fs.readFile('./views/cars.html', 'utf8', function (errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+
+    else if(request.url === '/cats') {
+        fs.readFile('./views/cats.html', 'utf8', function (errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if(request.url === '/cars/new') {
+        fs.readFile('./views/cars_new.html', 'utf8', function (errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+
+    else if(request.url === '/styles/main.css'){
+        fs.readFile('./styles/main.css', 'utf8', function(errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-type': 'text/css'});
+            response.write(contents);
+            response.end();
+        })
+    }    
+
+    else if(request.url === '/images/catt.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/catt.jpg', function(errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }    
+    else if(request.url === '/images/cat2.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cat2.jpg', function(errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }    
+    else if(request.url === '/images/cat3.jpg'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/cat3.jpg', function(errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-type': 'image/jpg'});
+            response.write(contents);
+            response.end();
+        })
+    }    
+
+    else if(request.url === '/images/lambo.png'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/lambo.png', function(errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-type': 'image/png'});
+            response.write(contents);
+            response.end();
+        })
+    }    
+    else if(request.url === '/images/ferrari.gif'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/ferrari.gif', function(errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-type': 'image/gif'});
+            response.write(contents);
+            response.end();
+        })
+    }    
+    else if(request.url === '/images/bugatti.png'){
+        // notice we won't include the utf8 encoding
+        fs.readFile('./images/bugatti.png', function(errors, contents){
+            if (errors) { console.log(errors); }
+            response.writeHead(200, {'Content-type': 'image/png'});
+            response.write(contents);
+            response.end();
+        })
+    }    
+
+    else { // request didn't match anything:                                        
+        response.writeHead(404);
+        response.end('File not found!!!');
+    }
+});
+// tell your server which port to run on
+server.listen(8000);
+// print to terminal window
+console.log("Running in localhost at port 8000");
--- test1/landing.js
+++ test1/landing.js
@@ -1,13 +1,3 @@
-// Assignment: Landing Page
-// Create a small node server capable of handling the following request URLs:
-// localhost:6789/    
-// This route should serve a view file called index.html and display a greeting.
-// localhost:6789/ninjas    
-// This route should serve a view file called ninjas.html and display information about ninjas.
-// localhost:6789/dojos/new    
-// This route should serve a view file called dojos.html and have a form (don't worry about where the form should be sent to).
-// If the URL is anything other than the ones above, have an error page load saying that the URL requested is not available.
-
 // get the http module:copy
 var http = require('http');
 // fs module allows us to read and write content for responses!!
@@ -17,8 +7,9 @@ var server = http.createServer(function (request, response){
     // see what URL the clients are requesting:
     console.log('client request URL: ', request.url);
     // this is how we do routing:
-    if(request.url === '/') {
+    if(request.url === '/') { // request.method == 'GET'
         fs.readFile('index.html', 'utf8', function (errors, contents){
+            if (errors) { console.log(errors); }
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.write(contents);
             response.end();
@@ -26,6 +17,7 @@ var server = http.createServer(function (request, response){
     }
     else if(request.url === '/ninjas') {
         fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+            if (errors) { console.log(errors); }
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.write(contents);
             response.end();
@@ -33,6 +25,7 @@ var server = http.createServer(function (request, response){
     }
     else if(request.url === '/dojos/new') {
         fs.readFile('dojos.html', 'utf8', function (errors, contents){
+            if (errors) { console.log(errors); }
             response.writeHead(200, {'Content-Type': 'text/html'});
             response.write(contents);
             response.end();
@@ -40,6 +33,7 @@ var server = http.createServer(function (request, response){
     }
     else if(request.url === '/css/main.css'){
         fs.readFile('./css/main.css', 'utf8', function(errors, contents){
+            if (errors) { console.log(errors); }
             response.writeHead(200, {'Content-type': 'text/css'});
             response.write(contents);
             response.end();
@@ -48,6 +42,7 @@ var server = http.createServer(function (request, response){
     else if(request.url === '/img/pizza.jpg'){
         // notice we won't include the utf8 encoding
         fs.readFile('./img/pizza.jpg', function(errors, contents){
+            if (errors) { console.log(errors); }
             response.writeHead(200, {'Content-type': 'image/jpg'});
             response.write(contents);
             response.end();
