0ed9c43203bac05be15a7babc4cd4bb783df02b8
Barton Schaefer <shoredata@gmail.com>
2018-07-05 16:26:40 -0700

    Express via ejs.

--- cars_and_cats_express/server.js
+++ cars_and_cats_express/server.js
@@ -8,8 +8,8 @@ var app = express();
 // console.log("Let's find out what app is", app);
 
 // This sets the location where express will look for the ejs views
-// app.set('views', __dirname + '/views'); 
-app.set('views', __dirname); 
+app.set('views', __dirname + '/views'); 
+console.log("views located: " + __dirname + "/views");
 
 // Now lets set the view engine itself so that express knows that we are using ejs 
 // as opposed to another templating engine like jade
@@ -18,31 +18,34 @@ app.set('view engine', 'ejs');
 // this is the line that tells our server to use the "/static" folder for static 
 // content
 app.use(express.static(__dirname + "/static"));
+console.log("static located: " + __dirname + "/static");
 // two underscores before dirname
 // try printing out __dirname using console.log to see what it is and why we 
 // use it
-console.log("Serving content for: " + __dirname.toString() + "\n   \
-from: " + __dirname.toString() + "/static")
 
-// use app's get method and pass it the base route '/' and a callback
-app.get('/', function(request, response) {
-    // just for fun, take a look at the request and response objects
-   console.log("The request object", request);
-   console.log("The response object", response);
-   // use the response object's .send() method to respond with an h1
-   response.send("<h1>Hello Express</h1>");
+app.get("/cars", function (request, response){
+    // hard-coded user data
+    var cars_array = [
+        {filename: "/images/bugatti.png"}, 
+        {filename: "/images/ferrari.gif"}, 
+        {filename: "/images/lambo.png"} 
+    ];
+    response.render('cars', {cars: cars_array});
+})
+app.get("/cats", function (request, response){
+    // hard-coded user data
+    var cats_array = [
+        {filename: "/images/catt.jpg"}, 
+        {filename: "/images/cat2.jpg"}, 
+        {filename: "/images/cat3.jpg"} 
+    ];
+    response.render('cats', {cats: cats_array});
+})
+app.get("/form", function (request, response){
+    // hard-coded user data
+    response.render('form', {});
 })
 
-// app.get("/users", function (request, response){
-//     // hard-coded user data
-//     var users_array = [
-//         {name: "Michael", email: "michael@codingdojo.com"}, 
-//         {name: "Jay", email: "jay@codingdojo.com"}, 
-//         {name: "Brendan", email: "brendan@codingdojo.com"}, 
-//         {name: "Andrew", email: "andrew@codingdojo.com"}
-//     ];
-//     response.render('users', {users: users_array});
-// })
 
 
 // tell the express app to listen on port 8000, always put this at the end 
