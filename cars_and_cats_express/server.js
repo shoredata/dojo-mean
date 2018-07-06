57cef40414512f3b413c5ca2ce1fcc4804e3c506
Barton Schaefer <shoredata@gmail.com>
2018-07-05 17:19:00 -0700

    Adding routes and views.

--- cars_and_cats_express/server.js
+++ cars_and_cats_express/server.js
@@ -23,26 +23,52 @@ console.log("static located: " + __dirname + "/static");
 // try printing out __dirname using console.log to see what it is and why we 
 // use it
 
+// app.get('/p/:tagId', function (req, res) {
+//     res.send("tagId is set to " + req.params.tagId);
+// });
+// // GET /p/5
+// // tagId is set to 5
+
+
+var cars_array = [
+    { filename: "/images/bugatti.png" },
+    { filename: "/images/ferrari.gif" },
+    { filename: "/images/lambo.png" }
+];
 app.get("/cars", function (request, response){
     // hard-coded user data
-    var cars_array = [
-        {filename: "/images/bugatti.png"}, 
-        {filename: "/images/ferrari.gif"}, 
-        {filename: "/images/lambo.png"} 
-    ];
     response.render('cars', {cars: cars_array});
 })
+
+
+var cats_array = [
+    { filename: "/images/catt.jpg", name:"Franklin", url:"/cats/0", number:1, id:0 },
+    { filename: "/images/cat2.jpg", name: "Forrest", url: "/cats/1", number:2, id:1},
+    { filename: "/images/cat3.jpg", name: "Fat Charley", url: "/cats/2", number:3, id:2 }
+];
+
 app.get("/cats", function (request, response){
     // hard-coded user data
-    var cats_array = [
-        {filename: "/images/catt.jpg"}, 
-        {filename: "/images/cat2.jpg"}, 
-        {filename: "/images/cat3.jpg"} 
-    ];
+    // console.log(request);
+    // console.log(response);
     response.render('cats', {cats: cats_array});
 })
+
+app.get("/cats/:catId", function (request, response) {
+    // console.log(request);
+    // console.log(response);
+
+    console.log("catId is set to " + request.params.catId);
+    var thisctxcat = cats_array[request.params.catId];
+    console.log("thisCat is " + thisctxcat.name, "ID #" + thisctxcat.id);
+    response.render("cat", { cats: cats_array, thiscat: thisctxcat } );
+});
+// // GET /cats/5
+// // catId is set to 5
+
+
 app.get("/form", function (request, response){
-    // hard-coded user data
+    // hard-coded user data in view
     response.render('form', {});
 })
 
@@ -51,5 +77,5 @@ app.get("/form", function (request, response){
 // tell the express app to listen on port 8000, always put this at the end 
 // of your server.js file
 app.listen(8000, function() {
-  console.log("listening on port 8000");
+    console.log("listening on port 8000");
 })
