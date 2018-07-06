2e60b3caec5c4a2be935e535d3c628c2785106d9
Barton Schaefer <shoredata@gmail.com>
2018-07-06 13:47:33 -0700

    Ninja Gold via Express per Alan.

--- misc/survey/server.js
+++ misc/survey/server.js
@@ -136,6 +136,125 @@ else {
 
 
 
+app.get('/ninjagold', function (req, res) {
+    console.log("**********\nNinja Gold Game");
+    // console.log("req.session = " + req.session);
+
+    if (req.session.gold_total==null) {
+        console.log("### SESSION TIMEOUT ########################################################################");
+        req.session.gold_total = 0;
+        req.session.actions = [];
+        req.session.save();
+    }
+    else if (typeof req.session.gold_total === 'undefined' || !req.session.gold_total) { 
+        console.log("*** SESSION TIMEOUT ***********************************************************************");
+        req.session.gold_total = 0;
+        req.session.actions = [];
+        req.session.save();
+    }
+
+    var gold_data = {
+        gold: req.session.gold_total,
+        actions: req.session.actions
+    }
+
+    res.render("ninjagold", { golddata: gold_data });
+})
+
+app.post('/ninjagold/update', function (req, res) {
+    console.log("**********\nNinjaGold Update, Post req.body = ", req.body);
+
+    if (req.session.gold_total == null) {
+        res.redirect('/ninjagold');
+        return;
+    }
+    else if (typeof req.session.gold_total === 'undefined' || !req.session.gold_total) { 
+        // res.redirect('/ninjagold');
+        // return;
+    }
+    else
+    {
+        // console.log(" ====== current: " + req.session.gold_total);
+    }
+    var limits = [];
+    var source = "";
+    var color = "";
+
+    if ('farm' in req.body) {
+        source = "Farm";
+        limits.push(10);
+        limits.push(20);
+    }
+    else if ('cave' in req.body) {
+        source = "Cave";
+        limits.push(5);
+        limits.push(10);
+    }
+    else if ('house' in req.body) {
+        source = "House";
+        limits.push(2);
+        limits.push(5);
+    }
+    else if ('casino' in req.body) {
+        source = "Casino";
+        limits.push(-50);
+        limits.push(50);
+    }
+    else if ('reset' in req.body) {
+        req.session.gold_total = 0;
+        req.session.actions = {};
+        req.session.save();
+        res.redirect('/ninjagold');
+        return;
+    }
+
+
+    var objToday = new Date(),
+        weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+        dayOfWeek = weekday[objToday.getDay()],
+        domEnder = function () { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+        dayOfMonth = stoday + (objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+        months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+        curMonth = months[objToday.getMonth()],
+        curYear = objToday.getFullYear(),
+        curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+        curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+        curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+        curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+    var stoday = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+
+
+    var amount = Math.floor(Math.random() * (limits[1] - limits[0])) + limits[0];
+    console.log("result = " + amount.toString());
+    req.session.gold_total += amount;
+    var sout = source + ", " + amount + " " + stoday;
+    var scolor = "";
+    if (amount > 0) {
+        sout = "Welcome to the " + source + "! You made " + amount.toString() + " gold!   " + stoday;
+        scolor = "green";
+    }
+    else if (amount<0) {
+        sout = "Oh Boy - In the " + source + " you lost " + Math.abs(amount).toString() + " gold.   " + stoday;
+        scolor = "red";
+    }
+    else {
+        sout = "Um, you broke even in the " + source + ".   " + stoday;
+        scolor = "yellow";
+    }
+    var thisaction = [];
+    thisaction.push(sout);
+    thisaction.push(scolor);
+    req.session.actions.splice(0, 0, thisaction);
+    req.session.save();
+
+    // console.log(req.session.actions);
+
+    res.redirect('/ninjagold');
+
+})
+
+
+
 // tell the express app to listen on port 8000
 app.listen(8000, function() {
  console.log("listening on port 8000");
